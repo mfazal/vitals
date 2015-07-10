3ffacfef00fdc1f70619b05e647ffb581c4ae47c
Faz <mfazal.q@gmail.com>
2015-07-11 00:53:19 +0400

    Added logic to convert SQLAlchemy result object to JSON

--- api-server/app/models.py
+++ api-server/app/models.py
@@ -1,5 +1,36 @@
+
+from flask import request, current_app
 from app import db
 import datetime
+import json
+
+
+class Serializer(object):
+  __public__ = None
+  "Must be implemented by implementors"
+
+  def to_serializable_dict(self):
+    dict = {}
+    for public_key in self.__public__:
+      value = getattr(self, public_key)
+      if value:
+        dict[public_key] = value
+    return dict
+
+class SWEncoder(json.JSONEncoder):
+  def default(self, obj):
+    if isinstance(obj, Serializer):
+      return obj.to_serializable_dict()
+    if isinstance(obj, (datetime.date)):
+      return obj.isoformat()
+    return json.JSONEncoder.default(self, obj)
+
+
+def SWJsonify(*args, **kwargs):
+  return current_app.response_class(json.dumps(dict(*args, **kwargs), cls=SWEncoder, indent=None if request.is_xhr else 2), mimetype='application/json')
+  # stolen from https://github.com/mitsuhiko/flask/blob/master/flask/helpers.py
+
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,14 +45,10 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
-    def __init__(self, nickname, email, posts, device, vitals, sos, notifs):
+    def __init__(self, nickname, email):
         self.nickname = nickname
         self.email = email
-        self.posts = posts
-        self.devices = device
-        self.vitals = vitals
-        self.sos = sos
-        self.notifications = notifs
+
 
 
 class Post(db.Model):
@@ -53,7 +80,8 @@ class Device(db.Model):
         self.type = type
         self.user_id = user_id
 
-class Vital(db.Model):
+class Vital(db.Model, Serializer):
+    __public__ = ['id','timestamp','user_id','tempinternal','tempexternal','heartrate','bloodoxy','baro']
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
@@ -63,9 +91,6 @@ class Vital(db.Model):
     bloodoxy = db.Column(db.Float(precision=4), default=0)
     baro = db.Column(db.Float(precision=4), default=0)
     
-    def __repr__(self):
-        return '<Vital %r>' % (self.user_id)
-
     def __init__(self, user_id, tempinternal, tempexternal, heartrate, bloodoxy, baro):
         self.timestamp = datetime.datetime.now()
         self.user_id = user_id
@@ -76,14 +101,15 @@ class Vital(db.Model):
         self.baro = baro
 
 
-class SOS(db.Model):
+class SOS(db.Model, Serializer):
+    __public__ = ['id','timestamp','user_id','sostxt']
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     sostxt = db.Column(db.String(200))
     
-    def __repr__(self):
-        return '<SOS %r>' % (self.user_id)
+    # def __repr__(self):
+    #     return '<SOS %r>' % (self.user_id)
 
     def __init__(self, user_id, sostxt):
         self.timestamp = datetime.datetime.now()
@@ -92,15 +118,16 @@ class SOS(db.Model):
 
 
    
-class Notification(db.Model):
+class Notification(db.Model, Serializer):
+    __public__ = ['id','timestamp','user_id','msg','mode']
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     msg = db.Column(db.Text)
     mode = db.Column(db.String(10))
     
-    def __repr__(self):
-        return '<Notification %r>' % (self.user_id)
+    # def __repr__(self):
+    #     return '<Notification %r>' % (self.user_id)
      
 
     def __init__(self, user_id, msg, mode):
